96ffada2a3a07d53456310447d40c7ca7a36b16f
Poojaa Ravi Kumar <150212234+poojaa-ravi@users.noreply.github.com>
2025-02-02 20:54:54 -0500

    Create 1251. Average Selling Price.sql
     LC

---
+++ 1251. Average Selling Price.sql
@@ -0,0 +1,6 @@
+-- Write your MySQL query statement below
+select p.product_id, COALESCE(ROUND(SUM(p.price * u.units)/SUM(u.units),2),0) AS average_price
+from prices p
+left join unitssold u on p.product_id = u.product_id and
+u.purchase_date between p.start_date and p.end_date 
+group by 1;
