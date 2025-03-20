2fd504853f030a67c55a3f4312f516c728284b78
Poojaa Ravi Kumar <150212234+poojaa-ravi@users.noreply.github.com>
2025-03-19 20:54:19 -0400

    [1070] [Time Beats: 90.81%] - LeetPush

---
+++ Product-Sales-Analysis-III.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select product_id, year as first_year, quantity, price 
+from sales
+where (product_id, year) in (select product_id, MIN(year) as year from sales group by product_id)
+order by 1
+
